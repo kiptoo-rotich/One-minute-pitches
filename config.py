a5f1350855d04917dab7ac8de8ba33641a172709
Softonic254 <rotichenoch27@gmail.com>
2021-06-14 13:36:51 +0300

    It create functions to update profile and upload pictures

---
+++ config.py
@@ -0,0 +1,36 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:qwerty@localhost/pitches'
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    #email configuration
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    UPLOADED_PHOTOS_DEST='app/static/photos'
+    
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
+class DevConfig(Config):
+    Debug = True
+
+
+class ProdConfig(Config):
+    pass
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
---
+++ start.sh
@@ -0,0 +1,5 @@
+export MAIL_USERNAME=enock.rotich@student.moringaschool.com
+export MAIL_PASSWORD=Softonnique27
+export SECRET_KEY=qwerty
+
+python3.8 manage.py server
