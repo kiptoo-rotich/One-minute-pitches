b1e9d8cbf62691180f199cb662a1609ef5a8b776
Softonic254 <rotichenoch27@gmail.com>
2021-06-14 17:39:01 +0300

    It will add id, title, comment , time and foreign-key columns to pitch model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from . import db
 from flask_login import UserMixin
 from . import login_manager
@@ -41,6 +42,28 @@ class Comment(db.Model):
 
 class Pitch(db.Model):
     __tablename__ = 'pitches'
-    
     id = db.Column(db.Integer, primary_key=True)
-    pitch = db.Column(db.String(255), nullable=False)
+    pitch_id = db.Column(db.Integer)
+    pitch_title = db.Column(db.String)
+    pitch_category = db.Column(db.String)
+    pitch_comment = db.Column(db.String)
+    posted = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    likes = db.Column(db.Integer)
+    dislikes = db.Column(db.Integer)
+
+    comments = db.relationship('Comment', backref='pitch', lazy="dynamic")
+
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_pitches(cls, category):
+        pitches = Pitch.query.filter_by(pitch_category=category).all()
+        return pitches
+
+    @classmethod
+    def getPitchId(cls, id):
+        pitch = Pitch.query.filter_by(id=id).first()
+        return pitch
