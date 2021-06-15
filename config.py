dc5f0bc255d1cfb59d139e2eaf4d025b213d73da
Softonic254 <rotichenoch27@gmail.com>
2021-06-15 14:34:28 +0300

    It fixes deployment

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://",1)
-
+    pass
 
 config_options = {
     'development': DevConfig,
