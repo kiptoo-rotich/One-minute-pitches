548aa9f7c938c1fce1d5f0e6d05dfe22d084c718
Softonic254 <rotichenoch27@gmail.com>
2021-06-15 13:43:04 +0300

    It fixes heroku error

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
     #email configuration
     MAIL_SERVER = 'smtp.googlemail.com'
